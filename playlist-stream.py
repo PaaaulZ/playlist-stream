603dc44986912b128a04742c013faa2643452306
PaaaulZ <skore.2o4@gmail.com>
2019-09-03 22:25:07 +0200

    Switched to classes to tidy up code and practice.

--- playlist-stream.py
+++ playlist-stream.py
@@ -4,73 +4,100 @@ import vlc
 import logging.config
 import os
 import datetime
+import time
+from unidecode import unidecode
 
-AVAILABLE_COMMANDS = "\np: Pause/Unpause\ns: Stop\nn: Next track\n"
-logger = None
+class PlaylistStream:
 
-def convert_time(seconds):
+    AVAILABLE_COMMANDS = "\np: Pause/Unpause\ns: Stop\nn: Next track\n++: Volume +10\n+: Volume +1\n--: Volume -10\n-: Volume -1\n\nCommand: "
+    logger = None
 
-    return str(datetime.timedelta(seconds=seconds))
+    def __init__(self):
 
-def clear_screen(): 
-  
-    # Windows clear screen
-    if os.name == 'nt': 
-        _ = os.system('cls') 
-    # Linux/Unix clear screeen
-    else: 
-        _ = os.system('clear') 
+        self.description = "Main class, contains script logic."
+        self.author = "PaaaulZ"
+        self.logger = logging.getLogger(__name__)
+        fh = logging.FileHandler('playlist-streamer.log')
+        logging.basicConfig(format='%(asctime)s - [%(levelname)s]: %(message)s', level=logging.DEBUG)
+        self.logger.addHandler(fh)
+        self.utils = Utils()
+        
+    def get_data(self,playlist_id,video_index):
 
-    return
+        options = {'outtmpl': '%(id)s%(ext)s'}
 
-def get_video_data(playlist_id,start_index,end_index):
+        if (video_index is not None):
+            options['playlistend'] = video_index
+            options['playliststart'] = video_index    
+    
+        downloader = youtube_dl.YoutubeDL(options)
 
-    data = {}
+        with downloader:
+            result = downloader.extract_info(playlist_id,download=False)
 
-    options = {'outtmpl': '%(id)s%(ext)s'}
-
-    if (start_index is not None):
-        options['playlistend'] = end_index
-    if (end_index is not None):
-        options['playliststart'] = start_index    
-   
-    downloader = youtube_dl.YoutubeDL(options)
-
-    with downloader:
-        result = downloader.extract_info(playlist_id,download=False)
-
-    if 'entries' in result:
-        # Can be a playlist or a list of videos
-        if len(result['entries']) > 0:
-            # Take the first video (end_index - start_index will always be 1 here), we load videos one by one.
-            video = result['entries'][0]
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            if len(result['entries']) > 0:
+                # Take the first video, we load videos one by one.
+                video = result['entries'][0]
+            else:
+                # Not enough videos?
+                raise IndexError("No results found. Starting/Ending index not in video count")
         else:
-            # Not enough videos?
-            raise IndexError("No results found. Starting/Ending index not in video count")
-    else:
-        # The URL is for a single video, not a playlist.
-        raise Exception("The URL given is not a playlist")
+            # The URL is for a single video, not a playlist.
+            raise Exception("The URL given is not a playlist")
+
+        description = self.utils.remove_non_ascii(video['description'])
+        title = self.utils.remove_non_ascii(video['title'])
+        uploader = self.utils.remove_non_ascii(video['uploader'])
+        duration = self.utils.convert_time(video['duration'])
+
+        for video_format in video['formats']:
+            if video_format['format_id'] == '140':
+                # 140 is m4a audio format.
+                url = video_format['url']
+                audio_data = AudioData(title,uploader,description,duration,url)
+                return audio_data
+
+        return None
 
 
-    data['description'] = video['description']
-    data['title'] = video['title']
-    data['uploader'] = video['uploader']
-    data['duration'] = convert_time(video['duration'])
+class AudioData:
 
-    for video_format in video['formats']:
-        if video_format['format_id'] == '140':
-            # 140 is m4a audio format.
-            data['url'] = video_format['url']
-            return data
+    def __init__(self,title,uploader,description,duration,url):
 
-    return None
+        self.title = title
+        self.uploader = uploader
+        self.description = description
+        self.duration = duration
+        self.url = url
+
+
+class Utils:
+
+    def convert_time(self,seconds):
+        return str(datetime.timedelta(seconds=seconds))
+    
+    def clear_screen(self): 
+        # Windows clear screen
+        if os.name == 'nt': 
+            _ = os.system('cls') 
+        # Linux/Unix clear screeen
+        else: 
+            _ = os.system('clear')             
+        return
+
+    def remove_non_ascii(self,text):
+        return unidecode(str(text))
 
 
 def main():
+
     parser = argparse.ArgumentParser("playlist-stream")
     parser.add_argument("-p", help="The youtube playlist URL", type=str)
     parser.add_argument("-s", help="Starting index (play videos after this number)", type=int)
     parser.add_argument("-e", help="Ending index (play videos before this number)", type=int)
+    parser.add_argument("-v", help="Starting volume (from 0 to 200)", type=int)
 
     args = parser.parse_args()
 
@@ -90,59 +117,83 @@ def main():
     if end_index is not None:
         if start_index > end_index:
             raise IndexError("Starting index id is greater than ending id")
-   
+
+    if args.v is not None:
+        if args.v < 0 or args.v > 200:
+            raise ValueError("Volume is too low or too high. Min is 0, Max is 200")
+        starting_volume = args.v
+    else:
+        starting_volume = 100
+
+
+    # Initialize objects (utilities and main object)
+    ps = PlaylistStream()
+
     # Initialize VLC player.
-    player = vlc.MediaPlayer()
+    player = vlc.MediaPlayer() 
+    player.audio_set_volume(starting_volume)
 
     while current_index <= end_index:
         
         try:
-            #video_data = get_video_data(playlist_id,current_index,end_index)
             # TODO: Remove second current_index (end_index) if it works
-            video_data = get_video_data(playlist_id,current_index,current_index)
-            audio_url = video_data['url']
-            uploader = video_data['uploader']
-            title = video_data['title']
-            duration = video_data['duration']
-            # Description for future use of timestamps
-            description = video_data['description']
+            data = ps.get_data(playlist_id,current_index)
         except youtube_dl.utils.DownloadError:
             # Can't download infos for this video, most of the times is geoblocked/private.
-            logger.warning(f"Skipping index {current_index} because the video is private or unknown error!")
+            ps.logger.warning(f"Skipping index {current_index} because the video is private or unknown error!")
             current_index += 1
             continue
 
         current_index += 1
 
         # Start playing
-        player.set_mrl(audio_url)
+        player.set_mrl(data.url)
         player.play()
 
 
         while player.is_playing:
-            clear_screen()
-            logger.info("---------")
-            logger.info(f"Currently playing {title} by {uploader} [{duration}]")   
-            selection = input(AVAILABLE_COMMANDS)
+
+            time.sleep(1)
+            ps.utils.clear_screen()
+            ps.logger.info("---------")
+            ps.logger.info(f"Currently playing {data.title} by {data.uploader} [{data.duration}]")   
+            ps.logger.info("---------")
+            selection = input(ps.AVAILABLE_COMMANDS)
+
             if selection in ('p','P'):
                 # Pause/Unpause player
                 if player.is_playing:
                     player.pause()
-                    logger.info("Player paused")
+                    ps.logger.info("Player paused")
                 else:
                     player.pause()
-                    logger.info("Player unpaused")
+                    ps.logger.info("Player unpaused")
             if selection in ('s','S'):
-                # Stops the player and closes the script
+                # Stop the player and closes the script
                 player.stop()
-                logger.info("Player stopped!")
+                ps.logger.info("Player stopped!")
                 exit()
             if selection in ('n','N'):
-                # Next track
-                player.stop()
+                # Next track (stop the player and skip this iteration)
+                player.pause()
                 break
+            if selection in ('d','D'):
+                ps.logger.debug(data.description)
+            if selection == '++':
+                player.audio_set_volume(player.audio_get_volume() + 10)
+                ps.logger.info(f"Volume +10 [{player.audio_get_volume()}]")
+            if selection == '+':
+                player.audio_set_volume(player.audio_get_volume() + 1)
+                ps.logger.info(f"Volume +1 [{player.audio_get_volume()}]")
+            if selection == '--':
+                player.audio_set_volume(player.audio_get_volume() - 10)
+                ps.logger.info(f"Volume -10 [{player.audio_get_volume()}]")
+            if selection == '-':
+                player.audio_set_volume(player.audio_get_volume() - 1)
+                ps.logger.info(f"Volume -1 [{player.audio_get_volume()}]")
             pass
 
+    # If we are out of this loop the playlist is over.
     logging.info("That was the last track. Stopping")
     player.stop()
     return
@@ -150,8 +201,5 @@ def main():
     
 
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    fh = logging.FileHandler('playlist-streamer.log')
-    logging.basicConfig(format='%(asctime)s - [%(levelname)s]: %(message)s', level=logging.DEBUG)
-    logger.addHandler(fh)
+
     main()
